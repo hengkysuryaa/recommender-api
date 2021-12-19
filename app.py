1660c148f8b6bf9b3f68a500aec95e37ad5bfe84
Hengky <49639099+hengkysuryaa@users.noreply.github.com>
2021-12-19 13:36:47 +0700

    feat: enable CORS

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
 from model import recommendation
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://bqsesvnewrsgch:40d5730752780492a3543f146fbd295483afc6e728200c494156deac64da4d95@ec2-50-19-171-158.compute-1.amazonaws.com:5432/dcenc4qk0na3sm"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -17,16 +19,18 @@ class User(db.Model):
 
 class Healtz(Resource):
     # Check API connection
-    
+    @cross_origin()
     def get(self):
         return jsonify({'message':'Hello World!'})
 
+    @cross_origin()
     def post(self):
         data = request.get_json()
         return jsonify({'message':f'Hello, {data.get("name")}!'})
 
 class Auth(Resource):
 
+    @cross_origin()
     def post(self):
         data = request.get_json()
         user = db.session.query(User).filter((User.username == data.get("username")) and (User.password == data.get("password"))).all()
@@ -35,6 +39,7 @@ class Auth(Resource):
 
 class Recommend(Resource):
 
+    @cross_origin()
     def post(self):
         data = request.get_json()
         ids, ratings, urls = recommendation(data.get("name"))
@@ -55,5 +60,5 @@ api.add_resource(Healtz, '/')
 api.add_resource(Auth, '/auth')
 api.add_resource(Recommend, '/recommend')
 
-# if __name__ == '__main__':
-#     app.run(debug = True)
+if __name__ == '__main__':
+    app.run(debug = True)
